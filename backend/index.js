84ba4e5057230825260edae28100ff376b57465c
pdh02189 <pdh02189@naver.com>
2024-11-22 15:14:45 +0900

    서버측 코드 추가  및 변경

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
-
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  // res.send('<h1>Hello World!</h1>')
+  res.sendFile(path.join(__dirname, "notice.html"));
 })
 
 app.listen(port, () => {
